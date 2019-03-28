1a009287db075353637024e780922779eb13bc4d
unknown <ed.beaudet@gmail.com>
2019-03-27 23:37:20 -0400

    fixed git issues

---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+$(document).on("click", "#scrapeButton", function () {
+    $.ajax({
+        method: "GET",
+        url: "/scrape",
+    })
+        .then(function (data) {
+            window.location = "/";
+        })
+});
+
+
+var articleId
+$(document).on("click", "#commentButton", function () {
+    var article = $(this).attr("data-id");
+    var thisarticle = article;
+    $("#commentModal").show();
+    articleId = thisarticle
+
+});
+
+// When you click the savecomment button
+$(document).on("click", "#saveButton", function () {
+    console.log("Comment: "+articleId);
+       // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + articleId,
+        data: {
+            comment: $("#comment").val()
+        }
+    })
+        // With that done
+        .then(function (data) {
+            // Log the response
+            console.log(data);
+            // Empty the notes section
+            $("#comment").empty();
+        });
+
+  $("#commentModal").hide();
+    
+});
